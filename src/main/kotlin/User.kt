0d3c069ecc7cd533333845036d331d4bf572faaa
LENOVO <test23062000@gmail.com>
2024-04-21 09:09:07 +0300

    Initial commit

---
+++ src/main/kotlin/User.kt
@@ -0,0 +1,17 @@
+data class User(
+    val username: String,
+    val email: String,
+    val age: Int,
+    val password: Int
+) : Verifiable {
+    override fun verifiable(classUser: User) {
+        if (classUser.username == username &&
+            classUser.email == email &&
+            classUser.age == age &&
+            classUser.password == password) {
+            println("Вход выполнен успешно!")
+        }else {
+            throw IllegalArgumentException("Неверные данные для входа")
+        }
+    }
+}
